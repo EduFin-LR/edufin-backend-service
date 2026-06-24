14f1a8ade33f06f3a330e08a09d7976084564764
DiegoR434 <u202123548@upc.edu.pe>
2026-06-24 16:31:13 -0500

    feat(gamification-module): added base functionality in domain layer.

---
+++ src/main/java/com/upc/edufinservice/gamification/infrastructure/persistence/jpa/repositories/GamificationProfileRepository.java
@@ -0,0 +1,15 @@
+﻿package com.upc.edufinservice.gamification.infrastructure.persistence.jpa.repositories;
+
+import com.upc.edufinservice.gamification.domain.model.aggregates.GamificationProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface GamificationProfileRepository extends JpaRepository<GamificationProfile, UUID> {
+    Optional<GamificationProfile> findByUserId(UUID userid);
+
+    boolean existByUserId(UUID userId);
+}
