cccd47e4e0de098e7c1e361490f92612ba37a1d2
DiegoR434 <u202123548@upc.edu.pe>
2026-06-25 04:27:59 -0500

    feat(iam-module): se agrego CORS.

--- src/main/java/com/upc/edufinservice/iam/infrastructure/hashing/bcrypt/SecurityConfig.java
+++ src/main/java/com/upc/edufinservice/iam/infrastructure/hashing/bcrypt/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -32,6 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors-> cors.configurationSource(corsConfigurationSource()))
                 // Desactivamos CSRF porque usaremos JWT (no necesitamos protección de formularios)
                 .csrf(AbstractHttpConfigurer::disable)
 
@@ -50,5 +56,25 @@ public class SecurityConfig {
                 .addFilterBefore(new JwtAuthenticationFilter(_tokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 1. Dominios permitidos (Aquí pones el puerto donde correrá tu React)
+        // Puedes agregar más si luego lo subes a la nube (ej. "https://mi-tesis.com")
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+
+        // 2. Métodos HTTP permitidos
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        // 3. Cabeceras permitidas (Crucial para que pase el Authorization: Bearer)
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        // 4. Aplicar a todas las rutas de tu API
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
